8f3f15051dfd7589a35815d2c3f3884dba1c224c
Ben Guler <benguler827@gmail.com>
2023-01-22 01:08:41 -0500

    commit 9999

--- src/components/Videos/components/Videos.js
+++ src/components/Videos/components/Videos.js
@@ -23,46 +23,54 @@ const App =()=>{
       sx={{
         marginLeft: 5,
       }}
-      >
+    >
+      <Grid container>
+        <Grid item xs={6} md={6}>
+          <Box
+            sx={{
+              width:500,
+            }}
+          >
+            <Typography variant="h1">VIDEOS</Typography>
+            <SearchBar onFormSubmit={search} setDisplayCount={setDisplayCount}/>
+          </Box>
 
-      <Box
-        sx={{
-          width:500,
-        }}
-      >
-        <Typography variant="h1">VIDEOS</Typography>
-        <SearchBar onFormSubmit={search} setDisplayCount={setDisplayCount}/>
-      </Box>
+          <Box
+            sx={{
+              marginTop: 3,
+            }}
+          >
+            <VideoDetail video={selectedVideo} />
+          </Box>
 
-      <Box
-        sx={{
-          marginTop: 3,
-        }}
-      >
-        <VideoDetail video={selectedVideo} />
-      </Box>
+        </Grid>
 
-      <Box
-        sx={{
-          marginTop: 5,
-          width:330,
-        }}
-      >
-        <VideoList onVideoSelect={(video) => setSelectedVideo(video)} videos={videos.slice(0, displayCount)}/>
-      </Box>
+        <Grid item xs={6} md={6}>
 
-      <Box
-        sx={{
-          marginTop: 3,
-          marginBottom: 3,
-          width:500,
-        }}
-      >
-        <LoadMore displayCount={displayCount} setDisplayCount={setDisplayCount}/>
-      </Box>
+          <Box
+            sx={{
+              marginTop: 28,
+              width:330,
+            }}
+          >
+            <VideoList onVideoSelect={(video) => setSelectedVideo(video)} videos={videos.slice(0, displayCount)}/>
+          </Box>
+
+          <Box
+            sx={{
+              marginTop: 3,
+              marginBottom: 3,
+              width:330,
+            }}
+          >
+            <LoadMore displayCount={displayCount} setDisplayCount={setDisplayCount}/>
+          </Box>
+
+        </Grid>
+
+      </Grid>
 
     </Box>
-
   );
 
 }
